e0238be441032ada6e58f9ede5ea0961d7705580
cami001 <ivettecamil@gmail.com>
2019-04-03 13:25:53 -0400

    Modulo De Mantenimientos

---
+++ WebApplication4/Controllers/CargosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class CargosController : Controller
+    {
+        private EmpleadosContext db = new EmpleadosContext();
+
+        // GET: Cargos
+        public ActionResult Index()
+        {
+            return View(db.Cargo.ToList());
+        }
+
+        // GET: Cargos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargos cargos = db.Cargo.Find(id);
+            if (cargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargos);
+        }
+
+        // GET: Cargos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cargos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,cargo")] Cargos cargos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cargo.Add(cargos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cargos);
+        }
+
+        // GET: Cargos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargos cargos = db.Cargo.Find(id);
+            if (cargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargos);
+        }
+
+        // POST: Cargos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,cargo")] Cargos cargos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cargos);
+        }
+
+        // GET: Cargos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargos cargos = db.Cargo.Find(id);
+            if (cargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargos);
+        }
+
+        // POST: Cargos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cargos cargos = db.Cargo.Find(id);
+            db.Cargo.Remove(cargos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/DepartamentosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private EmpleadosContext db = new EmpleadosContext();
+
+        // GET: Departamentos
+        public ActionResult Index()
+        {
+            return View(db.Departamento.ToList());
+        }
+
+        // GET: Departamentos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamentos departamentos = db.Departamento.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Codigo,Departamento,Nombre")] Departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departamento.Add(departamentos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamentos departamentos = db.Departamento.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: Departamentos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Codigo,Departamento,Nombre")] Departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamentos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamentos departamentos = db.Departamento.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Departamentos departamentos = db.Departamento.Find(id);
+            db.Departamento.Remove(departamentos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
